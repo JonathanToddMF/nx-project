fa7eb345ea464f8effd1164c2e19ba648cd161b8
Jonathan-Todd-MF <jonathan.todd@microfocus.com>
2022-07-06 17:09:35 +0100

    nx project

--- apps/nxng/src/app/app.module.ts
+++ apps/nxng/src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { provideDateTimeAdapter } from '@ux-aspects-angular/date-time';
+import { Theme, UxaThemeModule } from '@ux-aspects-angular/theming';
+import { UxaLuxonDateTimeAdapter } from '@ux-aspects-core/date-time-luxon';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    UxaThemeModule.forRoot(Theme.MicroFocus),
+  ],
+  providers: [
+    provideDateTimeAdapter(UxaLuxonDateTimeAdapter),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
